39e44fb6fa3f0524385492a9135f5df039b796f7
mvakkasoglu <merve.vakkasoglu@gmail.com>
2020-12-02 23:42:20 -0500

    added taking_input.py

--- taking_input.py
+++ taking_input.py
@@ -1,3 +1,23 @@
-print("Enter a value:")
-my_value = input()
-print("You entered {}".format(my_value))
+def method_input():
+    print("Enter a value:")
+    my_value = input()
+    print("You entered {}".format(my_value))
+
+
+def method_input_with_message():
+    my_value = input("Enter a value: ")
+    print("You entered ", my_value)
+
+
+def input_casing():
+    my_value = input("input a number: ")  # let’s input a number
+    print(type(my_value))  # prints <class 'str'>
+    my_value = int(input("input a number: "))  # using int() function
+    print(type(my_value))  # prints <class 'int'>
+    my_value = eval(input("input a number: "))  # using eval() function
+    print(type(my_value))  # prints <class 'int'>
+
+
+method_input()
+method_input_with_message()
+input_casing()
