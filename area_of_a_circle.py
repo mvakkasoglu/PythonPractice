ffd038b581d3943e5793fa84e6ffd97346eb10e9
mvakkasoglu <merve.vakkasoglu@gmail.com>
2020-12-02 23:12:10 -0500

    python exercises

---
+++ area_of_a_circle.py
@@ -0,0 +1,4 @@
+radius = 20.0
+area = radius * radius * 3.14159
+print("The area for the circle of radius {} is {}".format(radius, area))
+print("radius: ", radius)
---
+++ taking_input.py
@@ -0,0 +1,3 @@
+print("Enter a value:")
+my_value = input()
+print("You entered {}".format(my_value))
